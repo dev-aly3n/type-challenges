862bd862f7e9b48048e04388000454f22d12568f
aly3n <aly3n.dev@gmail.com>
2022-06-28 22:45:53 +0430

    add explanation to 00004 easy pick

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,4 +1,21 @@
+// answer:
 type MyPick<T, K extends keyof T> = {
     [P in K]: T[P]
 }
 
+// explanation:
+
+// the first T at the first line point to an object of types, like interfaces: 
+// object of types
+type x = {type1:number, type2:string};
+interface y {type1:number; type2:string;};
+
+// another keyword is keyof operator that gives us the key of an "object of types" as a union of strings
+type m = keyof x; // it's equal to type m = "type1" | "type2";
+type n = "type1" | "type2";
+// here m and n are equal
+
+// so (K extends keyof T) means: type K = "kye1" | "key2" | "key3" | ...
+//  the next keyword is "in". "in" operator in ts acts like js "in" operator, so it means if this P value exist in K object return true
+// and T[P] acts like js when we read a property from an object: obj["type1"]
+// finally it check if the P exist on K object it will use it as a key and also assign its type from K object.  
